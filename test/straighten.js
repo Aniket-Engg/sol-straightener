c0bcf6400bb0136edbebb8f143a84b14b46b9548
Aniket <aniket@Anikets-MacBook-Air.local>
2019-05-05 01:57:08 +0530

    linter added

--- test/straighten.js
+++ test/straighten.js
@@ -1,30 +1,32 @@
+'use strict';
+
 const Straightener = require('../'),
-    expect  = require('chai').expect;
-    fs = require('fs');
+  expect  = require('chai').expect,
+  fs = require('fs');
 
 describe('sol-straightener', () => {
 
-    it('Contract with single import', async () => {
-        let strtnFile = await Straightener.straighten(__dirname + '/contracts/SampleWithImport.sol');
-        let strtnContent = fs.readFileSync(__dirname + '/contracts/straightened/SampleWithImport.sol'); 
-        expect(Buffer.from(strtnFile)).to.deep.equal(strtnContent);
-    });
+  it('Contract with single import', async () => {
+    const strtnFile = await Straightener.straighten(__dirname + '/contracts/SampleWithImport.sol');
+    const strtnContent = fs.readFileSync(__dirname + '/contracts/straightened/SampleWithImport.sol');
+    expect(Buffer.from(strtnFile)).to.deep.equal(strtnContent);
+  });
 
-    it('Contract with nested imports', async () => {
-        let strtnFile = await Straightener.straighten(__dirname + '/contracts/SampleWithNestedImport.sol');
-        let strtnContent = fs.readFileSync(__dirname + '/contracts/straightened/SampleWithNestedImport.sol'); 
-        expect(Buffer.from(strtnFile)).to.deep.equal(strtnContent);
-    });
+  it('Contract with nested imports', async () => {
+    const strtnFile = await Straightener.straighten(__dirname + '/contracts/SampleWithNestedImport.sol');
+    const strtnContent = fs.readFileSync(__dirname + '/contracts/straightened/SampleWithNestedImport.sol');
+    expect(Buffer.from(strtnFile)).to.deep.equal(strtnContent);
+  });
 
-    it('Contract with multiple imports', async () => {
-        let strtnFile = await Straightener.straighten(__dirname + '/contracts/SampleWithMultiImport.sol');
-        let strtnContent = fs.readFileSync(__dirname + '/contracts/straightened/SampleWithMultiImport.sol'); 
-        expect(Buffer.from(strtnFile)).to.deep.equal(strtnContent);
-    });
+  it('Contract with multiple imports', async () => {
+    const strtnFile = await Straightener.straighten(__dirname + '/contracts/SampleWithMultiImport.sol');
+    const strtnContent = fs.readFileSync(__dirname + '/contracts/straightened/SampleWithMultiImport.sol');
+    expect(Buffer.from(strtnFile)).to.deep.equal(strtnContent);
+  });
 
-    it('Contract with imports from node_modules', async () => {
-        let strtnFile = await Straightener.straighten(__dirname + '/contracts/ImportFromNodeModules.sol');
-        let strtnContent = fs.readFileSync(__dirname + '/contracts/straightened/ImportFromNodeModules.sol'); 
-        expect(Buffer.from(strtnFile)).to.deep.equal(strtnContent);
-    });
+  it('Contract with imports from node_modules', async () => {
+    const strtnFile = await Straightener.straighten(__dirname + '/contracts/ImportFromNodeModules.sol');
+    const strtnContent = fs.readFileSync(__dirname + '/contracts/straightened/ImportFromNodeModules.sol');
+    expect(Buffer.from(strtnFile)).to.deep.equal(strtnContent);
+  });
 });
